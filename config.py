6cffdfc6a2a7779c30bfd507574809a0081b294a
James Geddes <j@jamesgeddes.pro>
2020-07-21 16:30:55 +0100

    Everything works now! Woop!

--- config.py
+++ config.py
@@ -1,12 +1,13 @@
+# Change these variables for your setup. Currently set to demo values.
+
 # The URL of your git repo
 git_url = "git@github.com:jamesgeddes/sandbox.git"
-
-# The text that git generates if there is no change in the repo.
-git_nochange = "Already up-to-date."
 
 # The path where your repo is stored locally
 git_local_path = "/media/james/Docs/Git/sandbox"
 
+# The name that you have given to your python script. ".py" is added for you.
+target_program = "helloworld"
 
-# The name that you have given to your target service. ".service" is added for you.
-target_program = "test-py"
+# How many seconds do you want to wait between pull?
+pull_wait = 60
--- main.py
+++ main.py
@@ -1,56 +1,45 @@
-from config import git_url, git_nochange, git_local_path, target_program
-from os import system
+from config import git_local_path, target_program
 from time import sleep
+import subprocess as sp
 import git
-import subprocess
 
-# https://www.devdungeon.com/content/working-git-repositories-python
-
-target_program += ".service"
-# git.remote.Remote(set)
-repo = git.Repo(git_local_path)
+target_program += ".py"
+git_local_path += "/"
 
 
-def git_pull_changed():
-    repo.remotes.origin.pull()
-    # check if something    changed
+def git_pull_change(path):
+    repo = git.Repo(path)
     current = repo.head.commit
 
     repo.remotes.origin.pull()
-    if current != repo.head.commit:
-        print("Repo changed! Activated.")
-        return True
-    else:
+
+    if current == repo.head.commit:
         print("Repo not changed. Sleep mode activated.")
         return False
+    else:
+        print("Repo changed! Activated.")
+        return True
 
 
-def target_restart():
-    # restart target
-    result = subprocess.run(['/usr/bin/systemctl daemon-reload'], stdout=subprocess.PIPE) # restart daemon
-    # result.stdout
-    # b'total 0\n-rw-r--r--  1 memyself  staff  0 Mar 14 11:04 files\n'
-
-    # The return value is a bytes object, so if you want a proper string, you'll need to decode it. Assuming the called process returns a UTF-8-encoded string:
-
-    print(result.stdout.decode('utf-8'))
-    # 'total 0\n-rw-r--r--  1 memyself  staff  0 Mar 14 11:04 files\n'
+def target_start(target):
+    #  Thanks! https://stackoverflow.com/questions/5611576/how-do-i-spawn-a-separate-python-process
+    return sp.Popen(["python3", git_local_path + target]).returncode  # assumes target in root of git directory
 
 
+def target_stop(target):
+    return sp.run(["pkill", "-f", target]).returncode
+
+
+def target_restart(target):
+    target_stop(target)
+    target_start(target)
+
+
+target_start(target_program)
 while True:
-    # changes = git_pull_changed()
-    # if changes:
-    target_restart()
-    sleep(60)  # runs roughly every minute. depends on everything else, but close enough for jazz.
+    if git_pull_change(git_local_path):
+        target_restart(target_program)
+    else:
+        pass
+    sleep(60)
 
-
-#
-#
-# cmd = ['echo', 'I like potatos']
-# proc = subprocess.Popen(cmd, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
-#
-# o, e = proc.communicate()
-#
-# print('Output: ' + o.decode('ascii'))
-# print('Error: '  + e.decode('ascii'))
-# print('code: ' + str(proc.returncode))
